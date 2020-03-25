e1eb6b93f56b4cdc2545772023a1f34be176db53
Raif Ahmed <rahmed@redhat.com>
2020-03-25 22:32:52 +0100

    update

--- gen-pdf.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-
-asciidoctor-pdf -r asciidoctor-diagram -a reportdir=$(pwd) \
-                -a ej-base-dir=$(git rev-parse --show-toplevel) \
-                 *.adoc
